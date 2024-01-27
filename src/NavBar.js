92a6d36879e7cc391ba383f6a8c8e968e8f63714
Daniel Kim <danieldkim.42@gmail.com>
2024-01-27 09:50:10 -0800

    passed down vis state var from app to navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,17 +1,23 @@
 import { Link } from "react-router-dom";
-import './NavBar.css'
+import './NavBar.css';
+import { useEffect } from "react";
 
-function NavBar() {
+function NavBar(props) {
+
+  useEffect(()=> {
+    console.log('props.vis: ', props.vis)
+  },[props.vis])
+
   return (
     <nav>
-      <ul id="primary-navigation" className="primary-navigation flex">
+      <ul id="primary-navigation" data-visible={props.vis ? "true" : "false"} className={`primary-navigation flex`}>
         <li className="active">
-          <Link to="/">
+          <Link to="/" className="ff-sans-cond uppercase text-white letter-spacing-2">
             <span aria-hidden="true">00</span>Home
           </Link>
         </li>
         <li>
-          <Link to="/projects">
+          <Link to="/projects" className="ff-sans-cond uppercase text-white letter-spacing-2">
             <span aria-hidden="true">01</span>Projects
           </Link>
         </li>
