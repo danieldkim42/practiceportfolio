6cdf2923d77ceecea30e938d5896523305c0602a
Daniel Kim <danieldkim.42@gmail.com>
2024-01-27 09:48:53 -0800

    added NavBar to header and created state variable to track pop-out display

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import { Routes, Route } from 'react-router-dom';
-
-import logo from './logo.svg';
-
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import NavBar from './NavBar';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [vis, setVis] = useState(false);
+
+  function onClickHandler() {
+    setVis(vis === false ? true : false);
+  }
+  
+
   return (
     <>
-      <NavBar />
+      <header className='primary-header flex'>
+        <div>
+          <img src='./samplelogo.svg' alt='logo' className='logo'></img>
+        </div>
+        <button className='mobile-nav-toggle' onClick={onClickHandler} aria-controls='primary-navigation' aria-expanded={vis ? 'true' : 'false'}>
+          <span className='sr-only'></span>
+        </button>
+        <NavBar vis={vis}/>
+      </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
